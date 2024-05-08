5f96ebb38a1ba030341201647234102c4a2f05ac
eemiljka <eemiljka@metropolia.fi>
2024-05-08 19:26:27 +0300

    new about us and UI modifications

---
+++ wp-content/themes/example-theme/aboutus.php
@@ -0,0 +1,45 @@
+<?php
+ /*
+ Template Name: About page
+ */
+?>
+
+<?php get_header(); ?>
+<div class="hero-two">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/hero-gh-two.jpg" alt="hero image">
+</div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <section class="about-section">
+            <div class="container">
+                <div class="about-us-text">
+                <h2>Welcome to Guitar Hero Couch, where nostalgia meets modern-day rockstar vibes!</h2>
+                <p>At Guitar Hero Couch, we're more than just a store – we're a haven for enthusiasts and anyone who still rocks out to the legendary tunes of Guitar Hero. Inspired by the spirit of the early 2000s basement gaming sessions, we're here to bring back those cozy vibes and channel the energy of those epic jam sessions.</p>
+
+                <p>Whether you're reliving the glory days of Guitar Hero or discovering the game for the first time, we've got you covered with a curated selection of clothing, accessories, guitars, and games. From vintage-inspired tees and hoodies to authentic game controllers and hard-to-find titles hoarded from thrift shops, our collection celebrates the essence of the game and the thrill of hitting those epic solos.</p>
+
+                <p>So grab your favorite guitar, sink into your comfiest couch, and let Guitar Hero Couch be your go-to destination for all things rock and roll. Because here, everyone's a rockstar.</p>
+
+                    <p>Let's turn up the volume and keep the spirit of Guitar Hero alive – one epic riff at a time!</p>
+                </div>
+            </div>
+            <div class="about-container">
+                <div class="contact-info">
+                <h3>Guitar Hero Couch</h3>
+                <p>1234 Rockstar Lane</p>
+                <p>Brooklyn, NY 11201</p>
+                <p>Phone: 555-555-5555</p>
+                <p>Email: guitarhero@couch.com</p>
+            </div>
+            <div class="map">
+                <iframe width="90%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-74.0823%2C40.5581%2C-73.8660%2C40.7393&amp;layer=mapnik&amp;marker=40.6480%2C-73.9485" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=12/40.6480/-73.9485">View Larger Map</a></small>
+            </div>
+            </div>
+        </section>
+    </main>
+</div>
+
+<?php get_footer(); ?>
+
+
